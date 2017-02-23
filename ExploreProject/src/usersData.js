a0bbdc9186572791b9351a3c508ef25ce97d8b2e
Louisa <louisaspicer@gmail.com>
2017-02-23 23:32:23 +0000

    User layout tidied up

--- ExploreProject/src/usersData.js
+++ ExploreProject/src/usersData.js
@@ -6,7 +6,8 @@ StyleSheet,
 Text,
 TouchableOpacity,
 View,
-ScrollView
+ScrollView,
+TouchableHighlight
 } from 'react-native';
 import axios from 'axios';
 import { defaultStyles } from './styles';
@@ -36,30 +37,55 @@ export default class Users extends Component {
 
   render() {
      return (
-      <View style={styles.container}>
+       <View style={ styles.container }>
+      <TouchableHighlight style={ styles.button } onPress={ () => this.props.navigator.pop() }>
+        <Text style={ styles.buttonText }>Go Back</Text>
+      </TouchableHighlight>
+      <Text style={ styles.heading }>USERS</Text>
         <ScrollView contentContainerStyle={styles.scrollContent}>
         {this.state.users.map((m,i) => <Text style={styles.textContainer} key={i}>
         Name: {m.firstName} {m.lastName}{"\n"}
         Age: {m.age}{"\n"}
-        
-
-
+        City: {m.city}
         </Text>)}
         </ScrollView>
       </View>
-   );
-  }
+       );
+      }
 }
 
   const styles = StyleSheet.create({
+
   container: {
     paddingTop: 20,         // start below status bar
   },
 
+  heading: {
+   paddingTop: 10,
+   fontWeight: 'bold',
+   textAlign: 'center',
+   fontSize:20,
+   marginBottom:10
+   },
+
+   button: {
+   height:20,
+   justifyContent: 'center',
+   backgroundColor: '#efefef',
+   alignItems: 'center',
+   justifyContent: 'center'
+   },
+
+   buttonText: {
+   fontSize:10
+ },
+
   textContainer: {
-    marginLeft: 10,
+    fontSize:10,
+    paddingTop: 2,
+    marginLeft: 2,
     marginBottom: 10,
-    height: (height - 20 - 20) / rows - 10,
+    height: (height - 20 - 20) / rows - 20,
     width: (width - 10) / cols - 10,
   },
 
